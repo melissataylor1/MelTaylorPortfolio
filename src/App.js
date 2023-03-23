50e1ec12751ffd7a62f0ff1811e33cfa6477d9a4
m1taylor <m1taylor@ryerson.ca>
2023-03-23 18:18:45 -0400

    setup index and base app js and incl react link in components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import Navbar from "./components/Nav";
-import About from "./components/pages/Home";
+import Home from "./components/pages/Home";
 import Works from "./components/pages/Works";
 import Contact from "./components/pages/Contact";
 import Resume from "./components/pages/Resume";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState('Home');
 
   const hanglePageChange = (event) => setCurrentPage(event.target.id);
 
   const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Home') {
+      return <Home />;
     }
     else if (currentPage === 'Portfolio') {
       return <Works />;
