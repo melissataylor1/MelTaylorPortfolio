56e6b043a16ade4b0e57d0ae5e1ca0d7322ff514
m1taylor <m1taylor@ryerson.ca>
2023-03-23 19:07:20 -0400

    fixed typos in app js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,37 @@ import Home from "./components/pages/Home";
 import Works from "./components/pages/Works";
 import Contact from "./components/pages/Contact";
 import Resume from "./components/pages/Resume";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
+
   const [currentPage, setCurrentPage] = useState('Home');
 
-  const hanglePageChange = (event) => setCurrentPage(event.target.id);
-
-  const renderPage = () => {
+  function renderNavChoice() {
     if (currentPage === 'Home') {
       return <Home />;
     }
-    else if (currentPage === 'Portfolio') {
+   if (currentPage === 'Portfolio') {
       return <Works />;
     }
-    else if (currentPage === 'Resume') {
+    if (currentPage === 'Resume') {
       return <Resume />;
     }
-    else {
+    if (currentPage === 'Contact') {
       return <Contact />;
     }
   };
-
+  
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <main>
-      <Navbar currentPage={ currentPage } hanglePageChange={ hanglePageChange } />
-      { renderPage() }
+      <Navbar currentPage={ currentPage } handlePageChange={ handlePageChange } />
+      { renderNavChoice() }
+      <Footer />
     </main>
   );
+
 }
+
+export default App;
