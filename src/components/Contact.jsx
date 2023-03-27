bc424efe958936a04a5b6ef563fffcbca55702bf
m1taylor <m1taylor@ryerson.ca>
2023-03-27 11:35:19 -0400

    created php for email submission in contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,18 +33,18 @@ function Contact() {
       </div>
 
       <div class="contact-form-wrapper  my-auto">
-        <form>
+      <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
           <div class="form-item">
-             <input className="text-slate-900" type="text" name="name" required></input>
-              <label className="text-slate-500">Name:</label>
+             <input className="text-slate-900" type="text" id="Name" name="Name" required></input>
+              <label for="Name" className="text-slate-500">Name:</label>
           </div>
           <div class="form-item">
-              <input className="text-slate-900" type="text" name="email" required></input>
-              <label className="text-slate-500">Email:</label>
+              <input className="text-slate-900" type="email" id="Email" name="Email" required></input>
+              <label for="Email" className="text-slate-500">Email:</label>
           </div>
           <div class="form-item">
-              <textarea className="text-slate-900" name="message" required></textarea>
-              <label className="text-slate-500">Message:</label>
+              <textarea className="text-slate-900" id="Message" name="Message" required></textarea>
+              <label for="Message" className="text-slate-500">Message:</label>
           </div>
           <button  className="border border-slate-50 text-slate-50 p-3 font-bold text-sm rounded w-1/3 mx-auto my-0 text-center hidden transition-all duration-500  md:block hover:contacthover">Send</button>  
         </form>
